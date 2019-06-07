aa442c2d6ef25ad803db41e426cd676c91062633
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-06 21:46:50 -0300

    Adiciona o arquivo app.js que será o arquivo que chamara todos os outros.

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react';
+
+const App = React.createClass({
+  render: function () {
+    return <h1>Aplicação Teste react hot loader!!!</h1>
+  }
+})
+
+export default App;
