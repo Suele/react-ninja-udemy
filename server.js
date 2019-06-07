ffb4a887c6d61feb37b084b650624cbc35537e47
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-06 21:50:14 -0300

    Adiciona o servidor do front-end.

---
+++ server.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const webpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+new webpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: {colors: true}
+}).listen(3000, (err) => {
+  if(err){
+    return console.log(err)
+  }
+  console.log('server run port 3000');
+})
