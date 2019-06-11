1dc4f0d3192f92dd239d98f18033442ef657cfe9
Suele Guimarães <suelen.ads.guimaraes@gmail.com>
2019-06-11 17:59:07 -0300

    Adiciona os elementod Button, LikeButton e SeachButton.

---
+++ src/SeachButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from './Button';
+
+const SeachButton = () => (
+	<Button handleClick={() => alert('Seach')}>seach</Button>
+);
+
+export default SeachButton;
