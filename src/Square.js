860758de2c4db0e27fd642c5b1636138148135c2
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-10 23:26:10 -0300

    Adiciona o elemento Square.

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = (props) => (
+  <div style={{
+    backgroundColor: props.color,
+    heigth: '100px',
+    width: '100px'
+  }}/>
+);
+
+export default Square;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-"use strict";
 
 import React from "react";
 import Title from "./Title";
+import Square from "./Square";
 
 class App extends React.Component {
   render() {
     return (
       <h1>
         <Title name='Suele ' lastname='Guimarães'/>
+        <Square color='blue'/>
       </h1>
     );
   }
